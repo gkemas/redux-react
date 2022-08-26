4cf526983cdf1e5362827144f12110f9d3a9d9c9
gokhan <kemasgokhan@gmail.com>
2022-08-26 20:16:30 +0300

    ad img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 const Card = ({name,sys,main,weather}) => {
-  return (
+  
+    return (
     <div>
-         <span>${name}</span>
-         <sup>${sys.country}</sup>
-        
-            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+         <span>{name}</span>
+         <sup>{sys.country}</sup>
+         
+         
+          
+           
+            <div class="city-temp">{Math.round(main.temp)}<sup>°C</sup></div>
             <figure>
-                {/* <img class="city-icon" src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png"/> */}
-                <figcaption>${weather[0].description}</figcaption>
-            </figure>`; 
+                <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}/>
+           
+              
+                <figcaption>{weather[0].description}</figcaption>
+            </figure> 
     </div>
   )
 }
