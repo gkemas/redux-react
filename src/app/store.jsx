2ccef308a25fd715b166a012c4873e1ee3b68610
gokhan <kemasgokhan@gmail.com>
2022-10-28 16:49:10 +0300

    as

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -5,7 +5,7 @@ import authReducer from '../features/authSlice';
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    
+    // fddf
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
